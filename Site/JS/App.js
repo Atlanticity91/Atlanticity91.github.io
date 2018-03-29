1b0fa6c10d134fc981837fdab5213382c443328e
Alves Quentin <atlanticity91@gmail.com>
2018-03-30 01:22:38 +0200

    Add files via upload

--- Site/JS/App.js
+++ Site/JS/App.js
@@ -1,5 +1,17 @@
 ( jQuery )( function( $ ) {
 
-  $( function( ) { $( '.sidenav' ).sidenav( ); } );
+  $( function( ) {
+
+    $( '.sidenav' ).sidenav( );
+    $( '.header-dropdown' ).dropdown( {
+        inDuration: 300,
+        outDuration: 225,
+        hover: false, // Activate on hover
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'right' // Displays dropdown with edge aligned to the left of button
+      }
+    );
+
+  } );
 
 } ); // end of jQuery name space
